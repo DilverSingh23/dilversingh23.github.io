9a2129b97292213b1619ff888a7e5b49a1978f20
DilverSingh23 <dsinghh23@gmail.com>
2024-10-02 22:52:07 -0400

    Revamp about me section
    Made experiences section drop down menu, added python as a language, and fixed spelling errors

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector(".hamburger");
 const links = document.querySelector(".links");
+const downArrows = document.querySelectorAll(".down-arrow");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    links.classList.toggle("active");
-})
+    if(getComputedStyle(links).display == "none") {
+        links.style.display = "flex";
+    }
+    else {
+        links.style.display = "none";
+    }
+});
+
+downArrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+        const description = arrow.parentElement.nextElementSibling;
+        if (getComputedStyle(description).display == "none") {
+            description.style.display = "flex";
+        }
+        else {
+            description.style.display = "none"
+        }
+    });
+});
