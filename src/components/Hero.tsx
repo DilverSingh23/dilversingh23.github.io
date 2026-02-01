961d0f4ddda9c0a626b3588e2a474c3ff656d46a
DilverSingh23 <dsinghh23@gmail.com>
2026-02-01 00:57:07 -0500

    feat: add hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,30 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Hero() {
     return (
-        <div className="min-h-screen w-full overflow-hidden bg-[url('/hero-bg.jpg')] bg-center bg-cover">
+        <div className="min-h-screen w-full flex flex-col overflow-hidden bg-[url('hero/hero-bg.jpg')] bg-center bg-cover">
+            <div className="flex flex-col flex-1 gap-12 justify-center items-center h-full w-full pt-16 h-[700px]:flex text-gray-300 font-inter">
+                <div>
+                    <img src="hero/profilepic.jpg" alt="Profile Image" 
+                    className="h-60 w-60 sm:h-76 sm:w-76 brightness-65 border-6 border-purple-200 rounded-full object-cover scale-95" />
+                </div>
+
+                <h1 className="text-5xl sm:text-6xl font-thin tracking-widest">Dilver Singh</h1>
+                <h1 className="text-sm min-[380px]:text-base sm:text-2xl font-extralight tracking-[0.03em] sm:tracking-[0.08em]">
+                    SWE Intern @ Miora | Aspiring Software Engineer
+                </h1>
+                
+                <div className="flex flex-row gap-6 text-gray-300 hover:cursor-pointer shadow-2xl">
+                    <a href="https://github.com/DilverSingh23" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="h-12 w-12 sm:h-16 sm:w-16 
+                        transition-all delay-150 duration-300 ease-in-out hover:text-white hover:scale-115" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/dilversingh" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="h-12 w-12 sm:h-16 sm:w-16
+                        transition-all delay-150 duration-300 ease-in-out hover:text-white hover:scale-115" />
+                    </a>
+                </div>
             
+            </div>
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+
     const navLinks = [
         { name: "About", onClick: null },
         { name: "Experience", onClick: null },
@@ -7,19 +10,31 @@ export default function Navbar() {
         { name: "Contact", onClick: null },
     ]
 
+    const [scrollY, setScrollY] = useState<number>(0)
+
+    useEffect(() => {
+        let rafID: number
+        function trackScroll() { rafID = requestAnimationFrame(() => setScrollY(window.scrollY)) }
+        window.addEventListener("scroll", trackScroll);
+
+        return () => {
+            cancelAnimationFrame(rafID)
+            window.removeEventListener("scroll", trackScroll)
+        }
+    }, [])
+
     return (
-        <nav className="fixed w-full flex gap-16 items-center justify-center backdrop-blur-md bg-black/1">
+        <nav className={`fixed w-full flex gap-16 items-center justify-center ${scrollY > 10 && "backdrop-blur-md bg-black/1"}`}>
             <img src="/logo.png" className="h-24 w-24" />
             <ul className="flex items-center gap-12 font-extralight text-white text-xl font-inter">
                 {navLinks.map((navLink: { name: string, onClick: any }) => (
                     <li className="border-b border-white/0 transition-all delay-100 duration-200 text-gray-300 
-                    ease-in-out hover:cursor-pointer hover:text-white hover:border-b-white " 
+                    ease-in-out text-shadow-lg hover:cursor-pointer hover:text-white hover:border-b-white" 
                     onClick={() => navLink.onClick}>
                         {navLink.name}
                     </li>
                 ))}
             </ul>
-
         </nav>
     )
 }
