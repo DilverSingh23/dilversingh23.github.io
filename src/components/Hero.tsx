3311935ecdd3aa5b0ba0968281f453e33e75b5dd
DilverSingh23 <dsinghh23@gmail.com>
2026-02-06 22:51:04 -0500

    refactor: fix navbar responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,14 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Hero() {
     return (
-        <div className="min-h-screen w-full flex flex-col overflow-hidden bg-[url('hero/hero-bg.jpg')] bg-center bg-cover">
+        <div className="min-h-screen w-full flex flex-col overflow-hidden bg-[url('hero/hero-bg.jpg')] bg-center bg-cover z-0 relative">
             <div className="flex flex-col flex-1 gap-12 justify-center items-center h-full w-full pt-16 h-[700px]:flex text-gray-300 font-inter">
                 <div>
                     <img src="hero/profilepic.jpg" alt="Profile Image" 
                     className="h-60 w-60 sm:h-76 sm:w-76 brightness-65 border-6 border-purple-200 rounded-full object-cover scale-95" />
                 </div>
 
-                <h1 className="text-5xl sm:text-6xl font-thin tracking-widest">Dilver Singh</h1>
+                <h1 className="text-5xl sm:text-6xl font-extralight tracking-widest">Dilver Singh</h1>
                 <h1 className="text-sm min-[380px]:text-base sm:text-2xl font-extralight tracking-[0.03em] sm:tracking-[0.08em]">
                     SWE Intern @ Miora | Aspiring Software Engineer
                 </h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
 
@@ -11,25 +12,32 @@ export default function Navbar() {
     ]
 
     const [scrollY, setScrollY] = useState<number>(0)
+    const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+    const [screenWidth, setScreenWidth] = useState<number>(0);
 
     useEffect(() => {
         let rafID: number
         function trackScroll() { rafID = requestAnimationFrame(() => setScrollY(window.scrollY)) }
+        function trackWidth () { setScreenWidth(window.innerWidth) }
+
+        window.addEventListener("resize", trackWidth);
         window.addEventListener("scroll", trackScroll);
 
         return () => {
             cancelAnimationFrame(rafID)
             window.removeEventListener("scroll", trackScroll)
+            window.removeEventListener("resize", trackWidth)
         }
     }, [])
 
     return (
-        <nav className={`fixed w-full flex gap-16 items-center justify-center ${scrollY > 10 && "backdrop-blur-md bg-black/1"}`}>
+        <nav className={`fixed w-full flex gap-6 md:gap-16 md:items-center justify-between md:justify-center max-md:px-6 z-50 ${scrollY > 10 && "backdrop-blur-md bg-black/1"}`}>
             <img src="/logo.png" className="h-24 w-24" />
-            <ul className="flex items-center gap-12 font-extralight text-white text-xl font-inter">
+            <RxHamburgerMenu className="md:hidden text-white self-center h-8 w-8" onClick={() => setHamburgerOpen((prev: boolean) => !prev)}/>
+            <ul className={`${hamburgerOpen && screenWidth < 768 ? "w-full bg-black/10 py-4 backdrop-blur-md flex absolute transform -translate-x-1/2 left-1/2 mt-24" : "hidden"} md:flex items-center gap-3 flex-col md:gap-12 md:flex-row font-extralight text-white text-xl font-inter z-50`}>
                 {navLinks.map((navLink: { name: string, onClick: any }) => (
-                    <li className="border-b border-white/0 transition-all delay-100 duration-200 text-gray-300 
-                    ease-in-out text-shadow-lg hover:cursor-pointer hover:text-white hover:border-b-white" 
+                    <li key={navLink.name} className="border-b border-white/0 transition-all delay-100 duration-200 text-gray-300 
+                    ease-in-out text-shadow-lg hover:cursor-pointer hover:text-white hover:border-b-white z-50" 
                     onClick={() => navLink.onClick}>
                         {navLink.name}
                     </li>
